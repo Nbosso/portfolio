2da6ca051bc0b322ed8ead11d087eea212f79141
Natan Santos Bosso <bosso.natan@gmail.com>
2022-10-25 22:47:10 -0300

    Changed name project

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'NB Web Page',
+      title: 'Portfólio',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
